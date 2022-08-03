71725971e36fd7717f94f835befabe3689a234a9
Samandar <samandarnfh.2020@gmail.com>
2022-08-03 12:18:46 +0500

    10

--- build_func10.py
+++ build_func10.py
@@ -1,8 +1,8 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
-x=2
+x=8
 y=4
-z=(3*(y**1/2)+x**(2/3))
+z=3*(y**(1/2)) + x**(2/3)
 answer=round(z,2)
-answer=pow(answer,3)
+#answer=pow(answer,3)
 print(answer)
